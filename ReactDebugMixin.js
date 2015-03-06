87f04132c32179d2ec3a7be6ab0f48078a5cad37
Lucas Carvalho <lucas.carvalho@corp.globo.com>
2015-03-06 11:25:02 -0300

    use strict, jslint and make it dry

--- ReactDebugMixin.js
+++ ReactDebugMixin.js
@@ -1,103 +1,110 @@
-var ReactDebugMixin = {
+/*global console*/
 
-  componentWillMount: function() {
-  	this._consoleCheck();
-	    this.debugUpdateCount = 0;
-	    time("MountTime")
-  },
+var ReactDebugMixin ;
+(function () {
+    'use strict';
 
-  debug: function(name, extra, collapse){
-  	// name must be string
-  	if(typeof name === "undefined"){
-  		err("ReactDebug: No name given: `this.debug(`componentName`, `stateKey`)`, defaulting to `componentName`")
-  	}
-  	this.componentName = name ? name : 'componentName';
-  	this.stateKey = extra ? extra : "_x_EMPTY_x_";
-  	this.collapsed = collapse ? true : false;
-  	this.collapsed ? grpC(this.componentName+" Will Mount: ", this.props) : grp(this.componentName+" Will Mount: ")
-  },
+    // Console Aliases
+    var log =     console.log             .bind(console);
+    var nfo =     console.info            .bind(console);
+    var time =    console.time            .bind(console);
+    var timeE =   console.timeEnd         .bind(console);
+    var grp =     console.group           .bind(console);
+    var grpE =    console.groupEnd        .bind(console);
+    var grpC =    console.groupCollapsed  .bind(console);
 
-  extra: function(){
-  	if (this.stateKey !== "_x_EMPTY_x_"){
-  		if (this.stateKey in this.state){
-  			nfo(this.stateKey+":", this.state[this.stateKey])
-  		} else {
-  			throw new Error("ReactDebug: `"+this.stateKey+"` does not exist on this component");
-  		}
-  	}
-  },
+    ReactDebugMixin = {
 
-  componentDidMount: function(){
-  	this.collapsed ? grpE(this.componentName+" Will Mount: ", this.props) : grpE(this.componentName+" Will Mount: ")
-  	this.collapsed ? grpC(this.componentName+" Did Mount: ", this.props) : grp(this.componentName+" Did Mount: ")
-  		this.debugmounted = true;
-  		timeE("MountTime")
-  		this.extra();
-  	this.collapsed ? grpE(this.componentName+" Did Mount: ", this.props) : grpE(this.componentName+" Did Mount: ")
-  },
+        componentWillMount: function() {
+          this._consoleCheck();
+              this.debugUpdateCount = 0;
+              time("MountTime");
+        },
 
-  componentWillReceiveProps: function(nextProps) {
-  	this.collapsed ? grpC(this.componentName+" Will Receive Props: ") : grp(this.componentName+" Will Receive Props: ")
-  		log("next Props: %O", nextProps)
-  		this.extra();
-  },
+        debug: function(name, extra, collapse){
+          this.componentName = name || 'componentName';
+          this.stateKey = extra || "_x_EMPTY_x_";
+          this.collapsed = collapse || false;
+          this.log("Wil Mount");
+        },
 
-  componentWillUpdate: function(nextProps, nextState){
-  	this.collapsed ? grpC(this.componentName+" Will Update: ", nextState) : grp(this.componentName+" Will Update: ")
-  		log("next Props: %O", nextProps)
-  		log("next State: %O", nextState)
-  		this.extra();
-	  	time("UpdateTime")
-  },
+        log: function log(message) {
+          if (this.collapsed === true) {
+            grpC(this.componentName + " " + message + ": ", this.props);
+          } else {
+            grp(this.componentName + " " + message + ": ");
+          }
+        },
 
-  componentDidUpdate: function(prevProps, prevState){
-  	grpE(this.componentName+" Will Receive Props: ")
-  	grpE(this.componentName+" Will Update: ")
+        extra: function(){
+          if (this.stateKey !== "_x_EMPTY_x_"){
+              if (this.stateKey in this.state){
+                  nfo(this.stateKey+":", this.state[this.stateKey]);
+              } else {
+                  throw new Error("ReactDebug: `"+this.stateKey+"` does not exist on this component");
+              }
+          }
+        },
 
-  	this.collapsed ? grpC(this.componentName+" Did Update: ", prevState) : grp(this.componentName+" Did Update: ")
-  		timeE("UpdateTime")
-	  	this.debugUpdateCount++;
-	    log("UpdateCounter: "+this.debugUpdateCount)
-	    log("previous Props: %O", prevProps)
-	    log("previous State: %O", prevState)
-	    this.extra();
-	grpE(this.componentName+" Did Update: ")
-  },
+        componentDidMount: function(){
+          this.log("Will Mount");
+          this.log("Did Mount");
+          this.debugmounted = true;
+          timeE("MountTime");
+          this.extra();
+          this.log("Did Mount");
+        },
 
-  componentWillUnmount: function() {
-  	this.collapsed ? grpC(this.componentName+" Will Unmount: ") : grp(this.componentName+" Will Unmount: ")
-    	log("Total UpdateCounter: "+this.debugUpdateCount)
-    	this.extra();
-    grpE(this.componentName+" Will Unmount: ")
-  },
-  _consoleCheck: function(){
-  	// Avoid `console` errors in browsers that lack a console.
-	var fallback = function() {};
-	var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'profile', 'profileEnd', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn', 'table'];
+        componentWillReceiveProps: function(nextProps) {
+          this.log("Will Receive Props");
+          log("next Props: %O", nextProps);
+          this.extra();
+        },
 
-	if(typeof console === 'undefined') {
-		var console = {};
-		console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time = console.timeEnd = console.assert = console.profile = function() {};
-	}
+        componentWillUpdate: function(nextProps, nextState){
+          this.log("Will Update");
+              log("next Props: %O", nextProps);
+              log("next State: %O", nextState);
+              this.extra();
+              time("UpdateTime");
+        },
 
-	for(var i in methods) {
-		if(typeof console[methods[i]] === 'undefined') {
-			console[methods[i]] = fallback;
-		}
-	}
-	// Console Aliases
-	var log =     console.log             .bind(console);
-	var nfo =     console.info            .bind(console);
-	var warn =    console.warn            .bind(console);
-	var err =     console.error           .bind(console);
-	var table =   console.table           .bind(console);
-	var dir =     console.dir             .bind(console);
-	var cnt =     console.count           .bind(console);
-	var time =    console.time            .bind(console);
-	var timeE =   console.timeEnd         .bind(console);
-	var grp =     console.group           .bind(console);
-	var grpE =    console.groupEnd        .bind(console);
-	var grpC =    console.groupCollapsed  .bind(console);
-  }
+        componentDidUpdate: function(prevProps, prevState){
+          grpE(this.componentName+" Will Receive Props: ");
+          grpE(this.componentName+" Will Update: ");
 
-};
+          this.log("Did Update");
+              timeE("UpdateTime");
+              this.debugUpdateCount++;
+              log("UpdateCounter: "+this.debugUpdateCount);
+              log("previous Props: %O", prevProps);
+              log("previous State: %O", prevState);
+              this.extra();
+          grpE(this.componentName+" Did Update: ");
+        },
+
+        componentWillUnmount: function() {
+            this.log('Will Unmount');
+              log("Total UpdateCounter: "+this.debugUpdateCount);
+              this.extra();
+          grpE(this.componentName+" Will Unmount: ");
+        },
+
+        _consoleCheck: function() {
+          // Avoid `console` errors in browsers that lack a console.
+          var fallback = function() {};
+          var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'profile', 'profileEnd', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn', 'table'];
+
+          if (typeof console === 'undefined') {
+              console.log = console.error = console.info = console.debug = console.warn = console.trace = console.dir = console.dirxml = console.group = console.groupEnd = console.time = console.timeEnd = console.assert = console.profile = function() {};
+          }
+
+          for(var i in methods) {
+              if(typeof console[methods[i]] === 'undefined') {
+                  console[methods[i]] = fallback;
+              }
+          }
+        }
+    };
+
+})();
